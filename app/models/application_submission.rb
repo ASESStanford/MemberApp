d97ec26cfb30ef45c95ece9bd65899e5539e75c4
David Jiang <dnjiang@stanford.edu>
2015-07-05 18:04:35 -0700

    Edited so app submission edits will add new questions

--- app/models/application_submission.rb
+++ app/models/application_submission.rb
@@ -11,12 +11,12 @@ class ApplicationSubmission < ActiveRecord::Base
   	#go through and create answers for every existing question in app form
   	all_questions = matching_qs
   	all_questions.each do |q|
-  		answers.create(question_id:q.id, text: "")
+  		answers.create(question_id:q.id, text: "") if !self.answers.pluck(:question_id).include?(q.id)
   	end
   end
 
   def grab_qas
-    init_answers if answers.size == 0
+    init_answers if answers.size != application_form.questions.size
     data = self.answers.map{|a| {a.question => a}} 
     return data
   end
