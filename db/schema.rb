0faaaa3d08f413240509a0f55af31b48e6d6685b
Stephen Ou <me@stephenou.com>
2015-07-09 21:01:43 -0700

    Change database schema with new submitted column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709205211) do
+ActiveRecord::Schema.define(version: 20150710033317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 20150709205211) do
   create_table "application_submissions", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "application_form_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "submitted",           default: false
   end
 
   add_index "application_submissions", ["application_form_id"], name: "index_application_submissions_on_application_form_id", using: :btree
