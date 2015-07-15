82207182371a4559080858bfc30c8748dca126f9
Stephen Ou <me@stephenou.com>
2015-07-15 14:21:37 -0700

    Fix wrong method name

--- app/controllers/interview_controller.rb
+++ app/controllers/interview_controller.rb
@@ -5,7 +5,7 @@ class InterviewController < ApplicationController
   	  @interviews = Interview.all
       @my_interviews = @new_interview.belong_to_interviewer(current_user.id)
     elsif current_user.is_reviewer?
-      @interviews = @new_interview.get_nil_reviewer
+      @interviews = @new_interview.get_nil_interviewer
       @my_interviews = @new_interview.belong_to_interviewer(current_user.id)
     else
       @interviews = @new_interview.get_available_interviews
