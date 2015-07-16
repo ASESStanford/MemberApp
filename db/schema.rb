b27f2b8935eda80555f7c1bc2fe73356acfc924b
Stephen Ou <me@stephenou.com>
2015-07-15 20:46:21 -0700

    Add migration and change schema for new email table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716011225) do
+ActiveRecord::Schema.define(version: 20150716034510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 20150716011225) do
 
   add_index "application_submissions", ["application_form_id"], name: "index_application_submissions_on_application_form_id", using: :btree
   add_index "application_submissions", ["user_id"], name: "index_application_submissions_on_user_id", using: :btree
+
+  create_table "emails", force: :cascade do |t|
+    t.text     "subject"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "interviews", force: :cascade do |t|
     t.datetime "time",           null: false
