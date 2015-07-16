34b90cf5879336ecde7bf54f662e513e88677db2
Stephen Ou <me@stephenou.com>
2015-07-16 14:09:53 -0700

    Get the email logic all working except for the actual Postmark call

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,2 +1,8 @@
 module EmailHelper
+	def substitute_name(text, user)
+		text.gsub! "$FIRSTNAME$", user.first_name
+		text.gsub! "$LASTNAME$", user.last_name
+		text.gsub! "$FULLNAME$", user.name
+		return text
+	end
 end
