4d6ceb88f4e5894078aa1086bc58a406ed5bfcf2
Stephen Ou <me@stephenou.com>
2015-07-26 15:15:34 -0700

    Fix syntax round 2

--- app/controllers/summit_controller.rb
+++ app/controllers/summit_controller.rb
@@ -16,6 +16,7 @@ class SummitController < ApplicationController
       :html_body => '<p>Hello ' + name + ',</p><p><strong>We have received your Summit application.</strong> We will get in touch with you soon about interviews.</p>',
       :track_opens => 'true'
     )
+  end
 
   def create
   	summit_form_id = 1
