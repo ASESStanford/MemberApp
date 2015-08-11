9d978d95213e991e79d4e3b3d7b22bd5b74df08a
Stephen Ou <me@stephenou.com>
2015-08-10 22:56:03 -0700

    Use Amazon S3 for file hosting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
-  config.logger = Logger.new(STDOUT)
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'ases-app',
+    :access_key_id => 'AKIAJHMSIXPHNCWSX55A',
+    :secret_access_key => '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'
+  }
+}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
