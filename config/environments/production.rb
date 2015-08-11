44a421e2d8474715acb51445a2cb198cd7e38db6
Stephen Ou <me@stephenou.com>
2015-08-11 16:21:36 -0700

    Stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,4 +94,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.gmail.com",
+    user_name: "asessummit2016@gmail.com",
+    password: "bestinhistory",
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
 end
